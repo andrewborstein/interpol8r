0c22f19559d9c4dd5c427390cdc04cba1e5d6b0a
Andrew Borstein <andrew@andrewborstein.com>
2018-06-03 21:26:02 -0400

    add some length validations for message attributes

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,11 +2,13 @@ class Message < ApplicationRecord
   SLUG_REGEX = /\A[a-z-]+[a-z]+\z/
 
   validates :name,
-    uniqueness: true,
     format: {
       with: SLUG_REGEX,
       message: 'can only contain lowercase letters and dashes'
-    }
+    },
+    length: 50,
+    uniqueness: true
+  validates :spec, length: 250
 
   def output
     Message::SpecRunner.new(spec).run
